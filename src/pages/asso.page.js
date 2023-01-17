f561796453b9ed4e40e70a86f85e6116be1568de
Mathieu <mathieu.blais@epitech.eu>
2023-01-17 21:55:27 +0100

    fix images

--- src/pages/asso.page.js
+++ src/pages/asso.page.js
@@ -9,14 +9,14 @@ export default function AssoPage({}) {
             <div className="lg:w-1/2 w-full p-5">
                 <div className="w-full h-fit flex items-center justify-center gap-10">
                     <a className="w-full h-full" href='https://www.mcpg.fr/'>
-                        <img className="w-[250px]" src="/mcpg+.png"/>
+                        <img className="w-[250px]" src="/mcpg++.png"/>
                     </a>
                     <a className="w-full h-full" href="https://discord.gg/WGvfubQ3G6">
-                        <img className="w-[200px]" src="/miokara.png"/>
+                        <img className="w-[200px]" src="/miokara2.png"/>
                     </a>
                 </div>
                 <p className="lg:text-6xl text-5xl font-grenze text-gray-200 mt-10">Une cité par MCPG+ et Miokara</p>
-                <img className="mt-10 rounded-xl" src='/mcpgdesc.jpg'>
+                <img className="mt-10 rounded-xl" src='/mcpgdescc.JPG'>
 
                 </img>
             </div>
