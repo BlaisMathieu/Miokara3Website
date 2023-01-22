1d5ca907094b7dece3d4eac9fca1a8e21769b421
Mathieu <mathieu.blais@epitech.eu>
2023-01-22 19:58:20 +0100

    add version to reglement

--- src/pages/reglement.page.js
+++ src/pages/reglement.page.js
@@ -8,7 +8,7 @@ export default function ReglementPage({}) {
                 <title>{'Cité de Miokara'}</title>
             </Helmet>
             <div className='hidden lg:flex w-full h-full mt-[55px]'>
-                <object data="./reglement.pdf" type="application/pdf" width="100%" height="100%"></object>
+                <object data="./reglement2.pdf" type="application/pdf" width="100%" height="100%"></object>
             </div>
             <div className='lg:hidden w-full h-full flex items-center justify-center p-5 text-center'>
                 <p className='text-3xl text-gray-200 font-grenze'>Merci d'ouvrir le site sur PC pour visualiser le réglement</p>
